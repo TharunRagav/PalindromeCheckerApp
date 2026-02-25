8d9ba6438752f4ae47a892701127bdab4fa7d07b
TharunRagav <mbtharunragavendran@gmail.com>
2026-02-25 16:24:57 +0530

    Added

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,13 +2,11 @@ public class PalindromeCheckerApp {
     public static void main(String[] args){
         String str1 = "madam";
         System.out.println("Input String: "+str1);
-        boolean isPalindrome =true;
-        for(int i=0;i<str1.length()/2;i++){
-            if(str1.charAt(i)!=str1.charAt(str1.length()-i-1)){
-                isPalindrome=false;
-                break;
-            }
+        String strReverse = "";
+        for(int i=str1.length()-1;i>=0;i--){
+            strReverse+=str1.charAt(i);
         }
-        System.out.println("Is it a Palindrome? "+(isPalindrome?"True":"False"));
+        System.out.println(strReverse);
+        System.out.println("Is it a Palindrome? "+(str1.equals(strReverse)?"True":"False"));
     }
 }
